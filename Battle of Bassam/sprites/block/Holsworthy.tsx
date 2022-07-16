7e9203b293ddc784d366fb9151ab6e3fb62f3bd9
CBT-19 <102010977+CBT-19@users.noreply.github.com>
2022-07-16 13:10:56 +1000

    Delete Battle of Bassam directory

--- Battle of Bassam/sprites/block/Holsworthy.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Holsworthy" tilewidth="20" tileheight="20" tilecount="6" columns="3">
- <image source="../../../../Downloads/6jdmcl.png" width="60" height="40"/>
-</tileset>
